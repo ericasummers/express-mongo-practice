0ac1dced80926380ee5a091273c9c5a892466109
Erica Wright <ericaw21@gmail.com>
2017-06-08 16:40:02 -0700

    add form to submit new user to db

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,38 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+  // Set internal DB variable
+  var db = req.db;
+
+  // Get form values relying on name attributes
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  //Set collection
+  var collection = db.get('usercollection');
+
+  //Submit to DB
+  collection.insert({
+    "username" : userName,
+    "email" : userEmail
+  }, function (err, doc) {
+    if (err) {
+      //If it fails return error
+      res.send("There was a problem adding the information to the database");
+    }
+    else {
+      //Send to success page
+      res.redirect("userlist");
+    }
+  });
+});
+
 module.exports = router;
